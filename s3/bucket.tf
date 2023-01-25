f3c7642e3d46e81a7931f3221f7180e6f9d38294
Alexander Eismont <alexander0390@hotmail.com>
2023-01-25 19:39:41 +0100

    added kms to bucket and backend for terragrunt

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,8 +1,28 @@
+terraform {
+  backend "s3" {}
+}
+
+resource "aws_kms_key" "test_alex_td_bucket_key" {
+  description             = "Tester terraform, kan slettes"
+  enable_key_rotation = true
+}
+
 resource "aws_s3_bucket" "test_alex_tf_bucket" {
     bucket = "${var.bucket_name}"
 }
 
-resource "aws_s3_bucket_acl" "test" {
+resource "aws_s3_bucket_acl" "test_alex_acl" {
   bucket = aws_s3_bucket.test_alex_tf_bucket.id
   acl = "${var.acl_value}"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.test_alex_tf_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.test_alex_td_bucket_key.id
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
